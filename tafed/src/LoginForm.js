0b7294815b31cacaeb37650b98ab2e16ee827e28
Malcolm Machesky <Malcolm9292@gmail.com>
2019-10-19 18:15:05 -0500

    form stuff

--- tafed/src/LoginForm.js
+++ tafed/src/LoginForm.js
@@ -9,7 +9,16 @@ function LoginForm(props) {
       initialValues={{ email: "", password: "" }}
       onSubmit={(values, { setSubmitting }) => {
         console.log("Submitting");
-      }}>
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email()
+          .required("Required"),
+        password: Yup.string()
+          .required("No password  provided.")
+          .min(8, "Password is too short - should be 8 chars minimum.")
+          .matches(/(?=.*[0-9])/, "Password must contain a number.")
+      })}>
       {props => {
         const {
           values,
@@ -23,15 +32,34 @@ function LoginForm(props) {
         return (
           <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label>
-            <input name="email" type="text" placeholder="Enter your email" />
-
+            <input
+              name="email"
+              type="text"
+              placeholder="Enter your email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={errors.email && touched.email && "error"}
+            />
+            {errors.email && touched.email && (
+              <div className="input-feedback">{errors.email}</div>
+            )}
             <label htmlFor="email">Password</label>
             <input
               name="password"
               type="password"
               placeholder="Enter your password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={errors.password && touched.password && "error"}
             />
-            <button type="submit">Login</button>
+            {errors.password && touched.password && (
+              <div className="input-Feedback">{errors.password}</div>
+            )}
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
           </form>
         );
       }}
