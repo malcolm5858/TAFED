6c601495c842e10b44d20a633766a180a6cbab14
Malcolm Machesky <Malcolm9292@gmail.com>
2019-10-19 17:42:10 -0500

    more react

--- tafed/src/LoginForm.js
+++ tafed/src/LoginForm.js
@@ -19,10 +19,21 @@ function LoginForm(props) {
           handleBlur,
           handleSubmit
         } = props;
+        return (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input name="email" type="text" placeholder="Enter your email" />
+
+            <label htmlFor="email">Password</label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+            <button type="submit">Login</button>
+          </form>
+        );
       }}
-      <div>
-        <h1> Login </h1>
-      </div>
     </Formik>
   );
 }
