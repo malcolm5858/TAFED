74dbcc1808c923afcd0bb0a2555f22f8d9c16b72
Malcolm Machesky <Malcolm9292@gmail.com>
2019-10-20 06:47:09 -0500

    commit

--- PythonServer/server.py
+++ PythonServer/server.py
@@ -50,11 +50,11 @@ class User:
 def initialize_stations(stations):
     f = open("CTA_-_System_Information_-_List_of__L__Stops.csv", "r")
     f_lines = f.readlines()
-
+    i = 1
     for f_line in f_lines[1:]:
         f_line_items = f_line.split(",")
         stations.append(Station(int(f_line_items[0]), f_line_items[1], f_line_items[2], f_line_items[3], f_line_items[6] == "true", f_line_items[7] == "true", f_line_items[8] == "true", f_line_items[9] == "true", f_line_items[10] == "true",
-                                f_line_items[11] == "true", f_line_items[12] == "true", f_line_items[13] == "true", f_line_items[14] == "true", f_line_items[15] == "true", float(f_line_items[16].split(",")[0][2:]), float(f_line_items[16].split(",")[1][1:-2])))
+                                f_line_items[11] == "true", f_line_items[12] == "true", f_line_items[13] == "true", f_line_items[14] == "true", f_line_items[15] == "true", float(f_line_items[16][2:]), float(f_line_items[17][1:-2])))
 
 
 def get_station_distance(station, latitude, longitude):
