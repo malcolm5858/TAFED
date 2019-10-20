4161bf221a6cc6c1366b50675ffbc601ae491228
Adrian Kirchner <akirchner@hawk.iit.edu>
2019-10-19 20:43:36 -0500

    created a station class

---
+++ PythonServer/station.py
@@ -0,0 +1,18 @@
+class Station:
+    def __init__(self, station_id, direction, stop_name, station_name, accessible, red, blue, green, brown, purple, purple_exp, yellow, pink, orange, latitude, longitude):
+        self.station_id = station_id
+        self.direction = direction
+        self.stop_name = stop_name
+        self.station_name = station_name
+        self.accessible = accessible
+        self.red = red
+        self.blue = blue
+        self.green = green
+        self.brown = brown
+        self.purple = purple
+        self.purple_exp = purple_exp
+        self.yellow = yellow
+        self.pink = pink
+        self.orange = orange
+        self.latitude = latitude
+        self.longitude = longitude
