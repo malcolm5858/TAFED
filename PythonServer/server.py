d1d39b1af9144e2cd49f28a3192724a1cec13228
Adrian Kirchner <akirchner@hawk.iit.edu>
2019-10-20 06:15:02 -0500

    combined everything and now there is only one file of import

--- PythonServer/server.py
+++ PythonServer/server.py
@@ -1,7 +1,8 @@
+import math
+
 from flask import Flask, request
 from flask_restful import Api, Resource, reqparse
 import psycopg2
-import PythonServer.main_api
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +13,96 @@ matches = []
 
 conn = None
 
+class Station:
+    def __init__(self, station_id, direction, stop_name, station_name, accessible, red, blue, green, brown, purple, purple_exp, yellow, pink, orange, latitude, longitude):
+        self.station_id = station_id
+        self.direction = direction
+        self.stop_name = stop_name
+        self.station_name = station_name
+        self.accessible = accessible
+        self.red = red
+        self.blue = blue
+        self.green = green
+        self.brown = brown
+        self.purple = purple
+        self.purple_exp = purple_exp
+        self.yellow = yellow
+        self.pink = pink
+        self.orange = orange
+        self.latitude = latitude
+        self.longitude = longitude
+
+class User:
+    def __init__(self, user_id, name, email, password, is_helper, need_accessibility, latitude, longitude):
+        self.user_id = user_id
+        self.email = email
+        self.password = password
+        self.name = name
+        self.is_helper = is_helper
+        self.need_accessibility = need_accessibility
+        self.latitude = latitude
+        self.longitude = longitude
+        self.match = None
+
+def initialize_stations(stations):
+    f = open("CTA_-_System_Information_-_List_of__L__Stops.csv", "r")
+    f_lines = f.readlines()
+
+    for f_line in f_lines[1:]:
+        f_line_items = f_line.split(",")
+        stations.append(Station(int(f_line_items[0]), f_line_items[1], f_line_items[2], f_line_items[3], f_line_items[6] == "true", f_line_items[7] == "true", f_line_items[8] == "true", f_line_items[9] == "true", f_line_items[10] == "true", f_line_items[11] == "true", f_line_items[12] == "true", f_line_items[13] == "true", f_line_items[14] == "true", f_line_items[15] == "true", float(f_line_items[16].split(",")[0][2:]), float(f_line_items[16].split(" ")[1][:-2])))
+
+def get_station_distance(station, latitude, longitude):
+    return math.sqrt((latitude - station.latitude) ** 2 + (longitude - station.longitude) ** 2)
+
+def get_user_distance(user1, user2):
+    return math.sqrt((user1.latitude - user2.latitude) ** 2 + (user1.longitude - user2.longitude) ** 2)
+
+def get_closest_station(stations, latitude, longitude):
+    closest = stations[0]
+    for station in stations:
+        if get_station_distance(station, latitude, longitude) < get_station_distance(closest, latitude, longitude):
+            closest = station
+    return closest
+
+def read_users(users, rows):
+    for row in rows:
+        add_user(row)
+
+def add_user(users, row):
+    users.append(User(int(row[0]), row[1], row[2], row[3], row[5] == "1", row[6] == "1"))
+
+def find_nearest_helper(users, helpee, excluded):
+    closest = None
+    for user in users:
+        if user not in excluded and user.is_helper:
+            if closest is None:
+                closest = user
+            elif get_user_distance(user, helpee) < get_user_distance(closest, helpee):
+                closest = user
+    return closest
+
+def match_helper(users, helpee):
+    if helpee.is_helper:
+        print("Not a helpee!")
+        return
+
+    helper = None
+    confirm = False
+    excluded = []
+    while not confirm:
+        helper = find_nearest_helper(users, helpee, excluded)
+        # TODO ask helper if they want to help and store the result in confirm
+        confirm = True
+        if not confirm:
+            excluded.append(helper)
+
+    return helper
+
+def search_user_email(users, email):
+    for user in users:
+        if user.email == email:
+            return user
 
 def fillInUsers():
     try:
