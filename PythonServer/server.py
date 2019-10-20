1e2a38918fa3986b4bdef5dbf0cf073c97a69ad0
Adrian Kirchner <akirchner@hawk.iit.edu>
2019-10-20 06:22:22 -0500

    smol fix

--- PythonServer/server.py
+++ PythonServer/server.py
@@ -54,7 +54,7 @@ def initialize_stations(stations):
     for f_line in f_lines[1:]:
         f_line_items = f_line.split(",")
         stations.append(Station(int(f_line_items[0]), f_line_items[1], f_line_items[2], f_line_items[3], f_line_items[6] == "true", f_line_items[7] == "true", f_line_items[8] == "true", f_line_items[9] == "true", f_line_items[10] == "true",
-                                f_line_items[11] == "true", f_line_items[12] == "true", f_line_items[13] == "true", f_line_items[14] == "true", f_line_items[15] == "true", float(f_line_items[16].split(",")[0][2:]), float(f_line_items[16].split(" ")[1][:-2])))
+                                f_line_items[11] == "true", f_line_items[12] == "true", f_line_items[13] == "true", f_line_items[14] == "true", f_line_items[15] == "true", float(f_line_items[16].split(",")[0][2:]), float(f_line_items[16].split(",")[1][1:-2])))
 
 
 def get_station_distance(station, latitude, longitude):
